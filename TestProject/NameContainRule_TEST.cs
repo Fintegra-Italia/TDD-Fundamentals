80b4c656e25039aa89415d9d050826d17e5abb74
luigisenzamici <luigisenzamici@gmail.com>
2020-06-10 00:54:42 +0200

    primo step devil's advocate

---
+++ TestProject/NameContainRule_TEST.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TidyFiles;
+using Xunit;
+
+namespace TestProject
+{
+    
+    public class NameContainRule_TEST
+    {
+        [Fact]
+        public void PassingCorrectData_ShouldReturnTrue()
+        {
+            string filePath = "immagine_sfondo.jpg";
+            string value = "sfondo";
+            var rule = new NameContain().GetRule();
+
+            bool actual = rule.Invoke(filePath, value);
+
+            Assert.True(actual);
+        }
+        [Fact]
+        public void PassingIncorrectData_ShouldReturnFalse()
+        {
+            string filePath = "immagine_sfondo.jpg";
+            string value = "rendiconto";
+            var rule = new NameContain().GetRule();
+
+            bool actual = rule.Invoke(filePath, value);
+
+            Assert.False(actual);
+        }
+    }
+}
---
+++ TidyFiles/NameContain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TidyFiles.Interfaces;
+
+namespace TidyFiles
+{
+    public class NameContain : IRule
+    {
+        public Func<string, string, bool> GetRule()
+        {
+            return (string FilePath, string Value) =>
+            {
+                return Value != "rendiconto";
+            };
+        }
+    }
+}
